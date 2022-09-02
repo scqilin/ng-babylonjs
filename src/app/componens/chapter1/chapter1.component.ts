cf37c5b0de13bee865f82ab638bbd34f967a3e95
scqilin <sql1211@qq.com>
2022-09-02 18:30:04 +0800

    1

--- src/app/componens/chapter1/chapter1.component.ts
+++ src/app/componens/chapter1/chapter1.component.ts
@@ -10,38 +10,40 @@ export class Chapter1Component implements OnInit {
   @ViewChild('renderCanvas') renderCanvas: any;
   @ViewChild('renderCanvas2') renderCanvas2: any;
   constructor() { }
-  
+
   ngOnInit(): void {
-    
+
   }
   ngAfterViewInit(): void {
-   
+
       this.initchar();
       this.initchar2();
 
   }
   initchar2(){
-  
+
     var canvas: any = this.renderCanvas2.nativeElement;
-  
+
     var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
-   
+
     var createScene = function () {
-     
+
       var scene = new BABYLON.Scene(engine);
-      
+
       const camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 3, 7, BABYLON.Vector3.Zero(),scene);
       camera.attachControl(canvas, true);
-      
+      camera.lowerRadiusLimit = 2;
+      camera.upperRadiusLimit = 100;
+
       const groundMat = new BABYLON.StandardMaterial("groundMat",scene);
       groundMat.diffuseTexture = new BABYLON.Texture("./assets/img/grasslight-big.jpg", scene);
-      
+      // groundMat.wireframe = true;
       var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-      
+
       var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, .2, scene, false, BABYLON.Mesh.FRONTSIDE);
       sphere.position.y = 1/2;
 
-      var ground = BABYLON.Mesh.CreateGround('ground1', 4, 4, 2, scene, false);
+      var ground = BABYLON.MeshBuilder.CreateGround('ground1', {width:4, height:4, subdivisions:2}, scene);
       ground.material = groundMat;
       const box = BABYLON.MeshBuilder.CreateBox("box", {size:1});
       const boxMat = new BABYLON.StandardMaterial("boxMat",scene);
@@ -56,67 +58,72 @@ export class Chapter1Component implements OnInit {
 
       sphere.parent = box2;
 
-     
+      scene.fogMode = BABYLON.Scene.FOGMODE_LINEAR;
+      scene.fogStart = 0.1;
+      scene.fogEnd = 10;
+      scene.fogDensity = 0.01;
+
+
       setInterval(() => {
         box.rotation.y += 0.03
-        box2.rotation.x += 1
+        box2.rotation.x += .1
       }, 30);
 
       return scene;
     }
-    
+
     var scene = createScene();
-    
+
     engine.runRenderLoop(function () {
       scene.render();
     });
-    
+
   }
   initchar(){
-  
+
     var canvas: any = this.renderCanvas.nativeElement;
-  
+
     var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
-   
+
     var createScene = function () {
-     
+
       var scene = new BABYLON.Scene(engine);
-      
+
       var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 3, -6), scene);
-      
+
       camera.setTarget(BABYLON.Vector3.Zero());
-      
+
       // camera.attachControl(canvas, false);
-      
+
       const groundMat = new BABYLON.StandardMaterial("groundMat",scene);
       groundMat.diffuseTexture = new BABYLON.Texture("./assets/img/grasslight-big.jpg", scene);
-      
+
       var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-      
-      var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 1, scene, false, BABYLON.Mesh.FRONTSIDE);
+
+      var sphere = BABYLON.MeshBuilder.CreateSphere('sphere1', {diameter:1}, scene);
       sphere.position.y = 1.5;
 
-      var ground = BABYLON.Mesh.CreateGround('ground1', 4, 4, 2, scene, false);
+      var ground = BABYLON.MeshBuilder.CreateGround('ground1', {width:4, height:4, subdivisions:2}, scene);
       ground.material = groundMat;
       const box = BABYLON.MeshBuilder.CreateBox("box", {size:1});
       const boxMat = new BABYLON.StandardMaterial("boxMat",scene);
       boxMat.diffuseTexture = new BABYLON.Texture("./assets/img/brick_diffuse.jpg", scene);
       box.material = boxMat;
       box.position.y = 0.5
-     
+
       setInterval(() => {
         box.rotation.y += 0.01
       }, 30);
 
       return scene;
     }
-    
+
     var scene = createScene();
-    
+
     engine.runRenderLoop(function () {
       scene.render();
     });
-    
+
   }
 
 }
